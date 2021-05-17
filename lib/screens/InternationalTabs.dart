bf9c9db21dae419f4b7cc7b39c34ae32cdd0f138
Vamsi Boggala <vamsiboggala@gmail.com>
2021-05-17 15:01:26 +0530

    cards implementation in InternationalTabs.dart

--- lib/screens/InternationalTabs.dart
+++ lib/screens/InternationalTabs.dart
@@ -9,9 +9,14 @@ class CardList extends StatefulWidget {
 }
 
 class _CardListState extends State<CardList> {
+  int _itemCount = 0;
+  void _incrementCounter() {
+    setState(() {
+
+      _itemCount++;
+    });
+  }
   @override
-
-
   Widget build(BuildContext context) {
     Widget allcards;
     return new Scaffold(
@@ -34,7 +39,7 @@ class _CardListState extends State<CardList> {
 
   }
 }
-
+@override
 Widget _card(BuildContext context){
   return new Card(
     shape: RoundedRectangleBorder(
@@ -45,9 +50,72 @@ Widget _card(BuildContext context){
       padding: const EdgeInsets.all(8.0),
       child: new Column(
         children: <Widget>[
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(5,5,1,10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1621095271433-2e4ec715c'
+                            '704?ixid=MnwxMjA3fDB8MHxwaG90by1'
+                            'wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=form'
+                            'at&fit=crop&w=634&q=80'
+                      ),
+                      radius: 25,
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Dawning Times',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.black87
+                          ),
+                        ),
+                        SizedBox(height: 5,),
+                        Text(
+                          'Posted 1 day ago',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black54
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                      child: new Padding(
+                        padding: new EdgeInsets.fromLTRB(1,1,1,1),
+                        /// Add Click Function
+                        child: new IconButton(
+                            icon: Icon(Icons.share_outlined),
+                        ),
+                      ),
+
+                  )
+                ],
+              ),
+            ),
+          ),
+          Divider(
+            color: Colors.black38,
+          ),
           new Image.network(
             /// Image Calling
-            'https://i.ytimg.com/vi/fq4N0hgOWzU/maxresdefault.jpg',
+            'https://images.unsplash.com/photo-1523215108660-3fdf7932d7a5?ixid='
+                'MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufD'
+                'B8fHx8&ixlib=rb-1.2.1&auto=forma'
+                't&fit=crop&w=1045&q=80',
+          ),
+          Divider(
+            color: Colors.black38,
           ),
           Container(
             child: Padding(
@@ -77,20 +145,76 @@ Widget _card(BuildContext context){
               ),
             ),
           ),
+          Divider(
+            color: Colors.black38,
+          ),
           new Container(
-              padding: new EdgeInsets.fromLTRB(1,15,1,5),
+              padding: new EdgeInsets.fromLTRB(1,5,1,5),
               child: new Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  new Padding(
-                    padding: new EdgeInsets.fromLTRB(1,1,5,1),
-                    ///Add ClickFunction
-                    child: new Icon(Icons.favorite_outline),
+                  Container(
+                    child: Row(
+                      children: [
+                         Container(
+                           child: new Padding(
+                            padding: new EdgeInsets.fromLTRB(1,1,1,1),
+                            ///Add ClickFunction
+                            child: new  IconButton(
+                              icon: Icon(
+                                Icons.thumb_up_alt_outlined,
+                                color: Colors.black87,
+                              ),
+                              iconSize: 25,
+
+                            ),
+                        ),
+                         ),
+                        Container(
+                          child: new Padding(
+                            padding: EdgeInsets.fromLTRB(1, 1, 1, 1),
+                            child: Text(
+                              '100',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.black87
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  new Padding(
-                    padding: new EdgeInsets.fromLTRB(1,1,5,1),
-                    /// Add Click Function
-                    child: new Icon(Icons.share_outlined),
+
+                  Container(
+                    child: Row(
+                      children: [
+                        new Padding(
+                          padding: new EdgeInsets.fromLTRB(1,1,1,1),
+                          ///Add ClickFunction
+                          child: new IconButton(
+                            icon: Icon(Icons.favorite_outline),
+                          )
+                        ),
+                        Container(
+                          child: new Padding(
+                            padding: new EdgeInsets.fromLTRB(1,1,1,1),
+                            /// Add Click Function
+                            child: new IconButton(
+                              icon: Icon(Icons.remove_red_eye_outlined),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: new Padding(
+                            padding: EdgeInsets.fromLTRB(1, 1, 1, 1),
+                            child: Text(
+                                '100'
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               )
@@ -100,6 +224,8 @@ Widget _card(BuildContext context){
     ),
   );
 }
+
+
 
 /*
 class CustomCard extends StatelessWidget {
